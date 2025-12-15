cda3ca2cc9f42888ff638d5d0ce1151be72314c9
joybappy2 <fan328547@gmail.com>
2025-12-15 08:56:46 +0600

    initial setup tailwind, daisyui, react-router

---
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import { Outlet } from "react-router";
+
+const MainLayout = () => {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-1 max-w-7xl mx-auto px-4 py-6">
+          <Outlet></Outlet>
+        </main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default MainLayout;
