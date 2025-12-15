0281fdaa54b093f8718767962b8da1ebc145b89d
joybappy2 <fan328547@gmail.com>
2025-12-15 09:12:32 +0600

    basic error page and authlayout routing

---
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthLayout = () => {
+  return (
+    <div>
+      <h2>Auth Layout</h2>
+    </div>
+  );
+};
+
+export default AuthLayout;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router";
 import MainLayout from "../layouts/MainLayout/MainLayout";
 import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import AuthLayout from "../layouts/AuthLayout/AuthLayout";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +14,26 @@ const router = createBrowserRouter([
         Component: Home,
       },
     ],
+  },
+
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "login",
+        Component: Login,
+      },
+    ],
+  },
+
+  {
+    path: "*",
+    element: (
+      <h2 className="text-7xl font-bold min-h-screen flex justify-center items-center">
+        Error 404
+      </h2>
+    ),
   },
 ]);
 
