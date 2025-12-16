47a3ceee9493092d3f678d36b114ff3855b02d42
joybappy2 <fan328547@gmail.com>
2025-12-16 09:19:04 +0600

    register page design

--- src/layouts/AuthLayout/AuthLayout.jsx
+++ src/layouts/AuthLayout/AuthLayout.jsx
@@ -5,9 +5,10 @@ import Logo from "../../components/Logo/Logo";
 const AuthLayout = () => {
   return (
     <div className="min-h-screen max-w-7xl mx-auto px-4 py-6">
-      <Logo className="text-4xl"></Logo>
-
-      <Outlet></Outlet>
+      <Logo className="text-4xl w-fit" />
+      <main className="mt-2">
+        <Outlet></Outlet>
+      </main>
     </div>
   );
 };
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -3,36 +3,81 @@ import Button from "../../components/Button/Button";
 
 const Register = () => {
   return (
-    <div>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-              <fieldset className="fieldset">
-                <label className="label">Email</label>
-                <input type="email" className="input" placeholder="Email" />
-                <label className="label">Password</label>
-                <input
-                  type="password"
-                  className="input"
-                  placeholder="Password"
-                />
-                <div>
-                  <a className="link link-hover">Forgot password?</a>
-                </div>
-                <Button className="btn-primary">Register</Button>
-              </fieldset>
+    <div className="flex justify-center items-center min-h-[calc(100vh-100px)]">
+      <div className="p-10 bg-base-100 rounded-xl w-sm shadow-xl">
+        <form className=" ">
+          <h2 className="text-2xl font-semibold text-center">Register</h2>
+          <fieldset className="fieldset">
+            {/* name */}
+            <label className="label">Name</label>
+            <input type="email" className="input w-full" placeholder="Name" />
+
+            {/* email */}
+            <label className="label">Email</label>
+            <input type="email" className="input w-full" placeholder="Email" />
+
+            {/* role */}
+            <label className="label">Role</label>
+            <select defaultValue="select a role" className="select">
+              <option disabled={true}>Select a Role</option>
+              <option value="">Teacher</option>
+              <option value="">Student</option>
+            </select>
+
+            {/* phone */}
+            <label className="label">Phone</label>
+            <input type="text" className="input w-full" placeholder="Phone" />
+            {/* password */}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              className="input w-full"
+              placeholder="Password"
+            />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
             </div>
-          </div>
+            <Button className="btn-primary">Register</Button>
+          </fieldset>
+        </form>
+
+        <div className="flex w-full flex-col">
+          <div className="divider">OR</div>
         </div>
+
+        {/* Google */}
+        <Button className="bg-base-100 border-primary w-full">
+          <span>
+            <svg
+              aria-label="Google logo"
+              width="16"
+              height="16"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+            >
+              <g>
+                <path d="m0 0H512V512H0" fill="#fff"></path>
+                <path
+                  fill="#34a853"
+                  d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"
+                ></path>
+                <path
+                  fill="#4285f4"
+                  d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"
+                ></path>
+                <path
+                  fill="#fbbc02"
+                  d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"
+                ></path>
+                <path
+                  fill="#ea4335"
+                  d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"
+                ></path>
+              </g>
+            </svg>
+          </span>
+          Login With Google
+        </Button>
       </div>
     </div>
   );
